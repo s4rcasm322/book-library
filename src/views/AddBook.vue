089f98ebbf9f8c724817db1f0227bc57c68fe026
Nikita Vasilenko <s4rcasm322@gmail.com>
2019-11-21 06:19:19 +0300

    Added validation

--- src/views/AddBook.vue
+++ src/views/AddBook.vue
@@ -1,7 +1,10 @@
 <template>
 	<div class="add-book">
 		<div class="container">
-			<h2 class="add-book__title">Добавлений новой книги</h2>
+			<header class="add-book__header">
+				<a class="add-book__back" href="#" @click.prevent="$router.push('/')">Назад</a>
+				<h2 class="add-book__title">Добавлений новой книги</h2>
+			</header>
 
 			<div class="add-book__content">
 				<photo-uploader class="add-book__upload" @upload="newBook.img = $event" @remove="newBook.img = ''" />
@@ -17,8 +20,7 @@
 						label-width="115"
 						label="Название"
 						placeholder="Название"
-						:error="false"
-						error-message="Поле должно быть заполнено!"
+						:error-message="errors.title ? errors.title : ''"
 					/>
 					<van-field
 						class="add-book__field"
@@ -30,7 +32,7 @@
 						label-width="115"
 						label="Имя автора"
 						placeholder="Имя автора"
-						error-message
+						:error-message="errors.firstName ? errors.firstName : ''"
 					/>
 					<van-field
 						class="add-book__field"
@@ -42,37 +44,38 @@
 						label-width="115"
 						label="Фамилия автора"
 						placeholder="Фамилия автора"
-						error-message
+						:error-message="errors.lastName ? errors.lastName : ''"
 					/>
 					<van-field
 						class="add-book__field"
 						v-model="newBook.publisher"
-						required
 						clearable
 						maxlength="30"
 						show-word-limit
 						label-width="115"
 						label="Издательство"
 						placeholder="Издательство"
-						error-message
 					/>
-					<van-field class="add-book__field" required clearable label-width="115" label="Год публикации" error-message>
+					<van-field
+						class="add-book__field"
+						label-width="115"
+						label="Год публикации"
+						:error-message="errors.publishDate ? errors.publishDate : ''"
+					>
 						<input
 							slot="input"
 							class="van-field__control"
 							v-model="newBook.publishDate"
-							type="number"
+							type="tel"
 							placeholder="Год публикации"
 							v-mask="'####'"
 						/>
 					</van-field>
 					<van-field
 						class="add-book__field"
-						required
-						clearable
 						label-width="115"
 						label="Дата выхода в тираж"
-						error-message
+						:error-message="errors.releaseDate ? errors.releaseDate : ''"
 					>
 						<input
 							slot="input"
@@ -83,7 +86,13 @@
 							v-mask="'##.##.####'"
 						/>
 					</van-field>
-					<van-field class="add-book__field" required clearable label-width="115" label="Кол-во страниц" error-message>
+					<van-field
+						class="add-book__field"
+						required
+						label-width="115"
+						label="Кол-во страниц"
+						:error-message="errors.pages ? errors.pages : ''"
+					>
 						<input
 							slot="input"
 							class="van-field__control"
@@ -128,7 +137,8 @@ export default {
 				releaseDate: '',
 				publisher: '',
 				img: ''
-			}
+			},
+			errors: {}
 		};
 	},
 	computed: {
@@ -142,18 +152,49 @@ export default {
 		}
 	},
 	methods: {
+		falidateForm() {
+			this.errors = {};
+			if (this.newBook.title.trim() === '') {
+				this.errors.title = 'Название должно быть заполнено!';
+			}
+
+			if (this.newBook.author.firstName.trim() === '') {
+				this.errors.firstName = 'Имя автора должно быть заполнено!';
+			}
+
+			if (this.newBook.author.lastName.trim() === '') {
+				this.errors.lastName = 'Фамилия автора должна быть заполнено!';
+			}
+
+			if (this.newBook.publishDate) {
+				if (Number(this.newBook.publishDate) < 1800) {
+					this.errors.publishDate = 'Год публикации должен быть не меньше 1800!';
+				}
+			}
+
+			if (new Date(this.newBook.releaseDate) < new Date('01.01.1800')) {
+				this.errors.releaseDate = 'Тираж не может датироваться ранее 01.01.1800!';
+			}
+
+			if (this.newBook.pages > 10000 || Number(this.newBook.pages) <= 0) {
+				this.errors.pages = 'Кол-во страниц не может быть меньше 0 и превышать 10000';
+			}
+		},
 		addBook() {
-			this.$spreloader.show();
+			this.falidateForm();
+			if (Object.entries(this.errors).length === 0) {
+				this.$spreloader.show();
 
-			setTimeout(() => {
-				this.newBook.id = this.books[this.books.length - 1].id + 1;
-				this.books.push(this.newBook);
-				window.localStorage.setItem('books', JSON.stringify(this.books));
+				setTimeout(() => {
+					this.newBook.id = this.books[this.books.length - 1].id + 1;
+					this.books.push(this.newBook);
+					window.localStorage.setItem('books', JSON.stringify(this.books));
 
-				this.$spreloader.hide();
+					this.$spreloader.hide();
 
-				this.$router.push('/');
-			}, 500);
+					this.$router.push('/');
+				}, 500);
+			}
 		}
 	},
 	mounted() {
@@ -161,7 +202,6 @@ export default {
 		if (localBooks) {
 			this.books = localBooks;
 		}
-		console.log(this.books);
 	}
 };
 </script>
@@ -170,8 +210,21 @@ export default {
 .add-book {
 	padding: 5rem 0;
 
-	&__title {
+	&__header {
+		display: flex;
+		justify-content: center;
+		align-items: center;
 		margin-bottom: 20px;
+	}
+
+	&__back {
+		margin-right: auto;
+		color: #00a086;
+		font-size: 20px;
+	}
+
+	&__title {
+		margin-right: auto;
 		text-align: center;
 		color: #444444;
 	}
