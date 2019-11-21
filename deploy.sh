7b99f4a56b382cf847a49ace7f29dac68fba58a4
Nikita Vasilenko <s4rcasm322@gmail.com>
2019-11-21 14:11:38 +0300

    Added deploy config file

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,7 @@
 set -e
 npm run build
 cd dist
-
 git add -A
 git commit -m 'deploy'
-
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 git push -f git@github.com:<s4rcasm322>/<book-library>.git master:gh-pages
 cd -
